c72d67898cdc275a74703d74781bc3506e86a718
liuzhouliang <525688067@qq.com>
2020-12-10 15:28:28 +0800

    Initial commit

---
+++ app/src/main/java/com/zinc/gradlestudy/MainActivity.java
@@ -0,0 +1,39 @@
+package com.zinc.gradlestudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        TestUtils.test();
+
+//        Test test = new Test();
+//        test.show(this);
+//
+//        int hi = test.hi(1, 4);
+//        ((TextView) findViewById(R.id.tv_content)).setText("江澎涌 zinc " + hi);
+//
+//        String name = BuildConfig.name;
+//        int age = BuildConfig.age;
+
+//        com.zinc.ZincPower zincPower = new com.zinc.ZincPower();
+//        int zinc = zincPower.hi(1, 4);
+//        zincPower.show(this, zinc + "");
+
+//        Jpy jpy = new Jpy();
+//        jpy.say();
+
+//        Xpy xpy = new Xpy();
+//        xpy.say();
+
+//        Zinc zinc1 = new Zinc();
+//        zinc1.say();
+
+    }
+}
---
+++ app/src/test/java/com/zinc/gradlestudy/JTest.java
@@ -0,0 +1,18 @@
+package com.zinc.gradlestudy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class JTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+        System.out.println(System.getProperty("zinc"));
+    }
+}
